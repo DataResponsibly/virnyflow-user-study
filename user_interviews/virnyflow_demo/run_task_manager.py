992a3ac61361fa0cb9425287e609310f938c9b0a
Denys Herasymuk <herasymuk@ucu.edu.ua>
2025-10-12 22:46:51 +0200

    Added Task Manager to docker-compose

---
+++ user_interviews/virnyflow_demo/run_task_manager.py
@@ -0,0 +1,17 @@
+import pathlib
+from virny_flow.task_manager import TaskManager
+from virny_flow.core.utils.common_helpers import create_exp_config_obj
+
+
+if __name__ == "__main__":
+    # Read an experimental config
+    exp_config_yaml_path = pathlib.Path(__file__).parent.joinpath('configs').joinpath('exp_config.yaml')
+    exp_config = create_exp_config_obj(exp_config_yaml_path=exp_config_yaml_path)
+
+    task_manager = TaskManager(secrets_path=exp_config.common_args.secrets_path,
+                               # host='127.0.0.1',
+                               host='0.0.0.0',
+                               port=8000,
+                               exp_config=exp_config,
+                               kafka_broker_address="kafka_broker:9092")
+    task_manager.run()
